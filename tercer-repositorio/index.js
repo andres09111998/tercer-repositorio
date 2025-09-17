597b83971165f550b2588e48fb240990df70c689
andres09111998 <andreesps4@gmail.com>
2025-09-17 15:48:32 -0500

    https://github.com/andres09111998/cuarto.git

---
+++ tercer-repositorio/index.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+/** 
+ Home is a component
+*/
+
+function Home(){
+    const listItems=[1,2,3,4]
+
+    const addItem=()=>{
+        const variableA = 1;
+        const variableB = 3;
+        const variableC = 2;
+
+        return variableA*3 +variableC*variableB
+    }
+    return(
+    <><div>Home</div>
+    <p>Hola mundo</p>
+    <buttin onclick={addItem}/></>
+    )
+    
+}
